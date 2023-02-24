fb40e13ae1e3257d10e7dafeb452288ac72c2024
lkmelberg <100446631+lkmelberg@users.noreply.github.com>
2023-02-24 14:32:51 +0100

    JSON task

--- js/index.js
+++ js/index.js
@@ -8,10 +8,28 @@
  * // Add two numbers together
  * const a = 1;
  * const b = 2;
- * const sum = okay(a, b);
+ * const sum = addNumbers(a, b);
  * // expect sum to be 3
  * ```
  */
-export function okay(a, b) {
+export function addNumbers(a, b) {
   return a + b;
+}
+
+/**
+ * subtracts num b from num a
+ * @param {number} a First value
+ * @param {number} b Second value
+ * @returns {number} Sum of params
+ * @example
+ * ```js
+ * // sumtract one number from another
+ * const a = 4;
+ * const b = 1;
+ * const sum = subtract(a, b);
+ * // expect sum to be 3
+ * ```
+ */
+export function subtract(a, b) {
+  return a - b;
 }
--- js/script.js
+++ js/script.js
@@ -1,35 +1,13 @@
-/**
- * Adds two numbers together
- * @param {number} a First value
- * @param {number} b Second value
- * @returns {number} Sum of params
- * @example
- * ```js
- * // Add two numbers together
- * const a = 1;
- * const b = 2;
- * const sum = addNumbers(a, b);
- * // expect sum to be 3
- * ```
- */
-export function addNumbers(a, b) {
-  return a + b;
-}
+const myCountry = {
+  country: "Norway",
+  city: "Oslo",
+  currency: "Krone",
+};
 
-/**
- * subtracts num b from num a
- * @param {number} a First value
- * @param {number} b Second value
- * @returns {number} Sum of params
- * @example
- * ```js
- * // sumtract one number from another
- * const a = 4;
- * const b = 1;
- * const sum = subtract(a, b);
- * // expect sum to be 3
- * ```
- */
-export function subtract(a, b) {
-  return a - b;
-}
+const stringify = JSON.stringify(myCountry);
+
+console.log(stringify);
+
+const parsed = JSON.parse(stringify);
+
+console.log(parsed);
